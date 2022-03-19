7936844b9e7cb2a65682fbbcde82f6f3cf4f9312
hemalraval <hemal8198@gmail.com>
2022-03-19 12:40:05 -0700

    FAQ and other changes

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// navbar togglemenu start
+var navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+    navLinks.style.right = "0";
+}
+
+function hideMenu() {
+    navLinks.style.right = "-200px";
+}
+// navbar togglemenu end
+
+
+
+// FAQ Section start
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+// FAQ Section end
